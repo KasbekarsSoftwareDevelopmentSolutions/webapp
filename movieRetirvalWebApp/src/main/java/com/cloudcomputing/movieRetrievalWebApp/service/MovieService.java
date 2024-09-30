b0988c8b230149e9dec7a725039f2a800f3310ba
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-09-30 17:10:12 -0400

    Implemented the DAO(Data Access Object) design pattern, in oder to separate the persistence logic and the business logic. Required changes made to MovieService.java and created MovieDAO.java with MovieDAOImpl.java.

--- movieRetirvalWebApp/src/main/java/com/cloudcomputing/movieRetrievalWebApp/service/MovieService.java
+++ movieRetirvalWebApp/src/main/java/com/cloudcomputing/movieRetrievalWebApp/service/MovieService.java
@@ -1,36 +1,36 @@
 package com.cloudcomputing.movieRetrievalWebApp.service;
 
-import com.cloudcomputing.movieRetrievalWebApp.repository.MovieRepo;
+import com.cloudcomputing.movieRetrievalWebApp.dao.MovieDAO;
+import com.cloudcomputing.movieRetrievalWebApp.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
 
     @Autowired
-    private MovieRepo movieRepo;
+    private MovieDAO movieDAO;
 
-//    public List<Movie> getAllMovies() {
-//        return movieRepo.findAll();
-//    }
-//
-//    public Optional<Movie> getMovieById(Long id) {
-//        return movieRepo.findById(id);
-//    }
-//
-//    public Movie addMovie(Movie movie) {
-//        return movieRepo.save(movie);
-//    }
-//
-//    public Movie updateMovie(Long id, Movie movieDetails) {
-//        Movie movie = movieRepo.findById(id).orElseThrow();
-//        movie.setTitle(movieDetails.getTitle());
-//        movie.setGenre(movieDetails.getGenre());
-//        movie.setReleaseYear(movieDetails.getReleaseYear());
-//        return movieRepo.save(movie);
-//    }
-//
-//    public void deleteMovie(Long id) {
-//        movieRepo.deleteById(id);
-//    }
+    public List<Movie> getAllMovies() {
+        return movieDAO.getAllMovies();
+    }
+
+    public Optional<Movie> getMovieById(Long id) {
+        return movieDAO.getMovieById(id);
+    }
+
+    public Movie addMovie(Movie movie) {
+        return movieDAO.addMovie(movie);
+    }
+
+    public Movie updateMovie(Long id, Movie movieDetails) {
+        return movieDAO.updateMovie(id, movieDetails);
+    }
+
+    public void deleteMovie(Long id) {
+        movieDAO.deleteMovie(id);
+    }
 }
