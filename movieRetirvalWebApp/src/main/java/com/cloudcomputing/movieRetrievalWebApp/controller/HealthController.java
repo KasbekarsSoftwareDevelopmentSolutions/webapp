1f37cf3669a3bed886ffd5031638d8c2fff9adfa
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-10-05 02:49:16 -0400

    Logger Statements added for debugging. toString() method added UserResponseDTO.java

--- movieRetirvalWebApp/src/main/java/com/cloudcomputing/movieRetrievalWebApp/controller/HealthController.java
+++ movieRetirvalWebApp/src/main/java/com/cloudcomputing/movieRetrievalWebApp/controller/HealthController.java
@@ -12,19 +12,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/healthz")
 public class HealthController {
+    private static final Logger LOGGER = Logger.getLogger(HealthController.class.getName());
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping
     public ResponseEntity<Void> healthCheck(HttpServletRequest request) {
-        // Check for query parameters
+        LOGGER.info("Health check endpoint accessed.");
         Map<String, String[]> queryParams = request.getParameterMap();
+
         if (request.getContentLength() > 0 || !queryParams.isEmpty()) {
+            LOGGER.warning("Invalid request: Content length is greater than zero or query parameters are present." + " ##HttpStatus.BAD_REQUEST sent in response## ");
             return ResponseEntity.badRequest()
                     .header("Cache-Control", "no-cache", "no-store", "must-revalidate")
                     .header("Pragma", "no-cache")
@@ -33,13 +37,16 @@ public class HealthController {
         }
 
         try {
+            LOGGER.info("Executing a simple database query to check connectivity.");
             jdbcTemplate.execute("SELECT 1");
+            LOGGER.info("Database connectivity check successful." + " ##HttpStatus.OK sent in response## ");
             return ResponseEntity.ok()
                     .header("Cache-Control", "no-cache", "no-store", "must-revalidate")
                     .header("Pragma", "no-cache")
                     .header("X-Content-Type-Options", "no-sniff")
                     .build();
         } catch (DataAccessException e) {
+            LOGGER.severe("Database connectivity check failed: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                     .header("Cache-Control", "no-cache", "no-store", "must-revalidate")
                     .header("Pragma", "no-cache")
@@ -50,6 +57,7 @@ public class HealthController {
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     public ResponseEntity<Void> methodNotAllowed() {
+        LOGGER.warning("Unsupported HTTP method attempted on /healthz endpoint.");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
                 .header("Cache-Control", "no-cache", "no-store", "must-revalidate")
                 .header("Pragma", "no-cache")
